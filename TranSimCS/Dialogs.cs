9074e9b26debaeac635d727a0205e11c6f80641a
Monniasza <37037712+Monniasza@users.noreply.github.com>
2025-09-24 20:09:11 +0200

    working on save and load dialogs

---
+++ TranSimCS/Dialogs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Eto.Forms;
+
+internal static class Dialogs {
+    public static DialogResult ShowWithFakeWindow(FileDialog dialog) {
+        DialogResult result = DialogResult.None;
+        Dialogs.UsingFakeWindow((form) => {
+            result = dialog.ShowDialog(form);
+        });
+        return result;
+    }
+    public static (SaveFileDialog, DialogResult) SaveDialog(Action<SaveFileDialog>? postsave, string? path = null) {
+        SaveFileDialog dialog = new SaveFileDialog();
+        var folder = path ?? Program.SaveDirectory;
+        dialog.Directory = new Uri(folder, UriKind.RelativeOrAbsolute);
+        DialogResult result = ShowWithFakeWindow(dialog);
+        return (dialog, result);
+    }
+    public static (OpenFileDialog, DialogResult) LoadDialog(Action<OpenFileDialog>? postload, string? path = null) {
+        OpenFileDialog dialog = new OpenFileDialog();
+        var folder = path ?? Program.SaveDirectory;
+        dialog.Directory = new Uri(folder, UriKind.RelativeOrAbsolute);
+        DialogResult result = ShowWithFakeWindow(dialog);
+        return (dialog, result);
+    }
+
+    public static void UsingFakeWindow(Action<Form> action) {
+        var fakeWindow = new Form();
+        fakeWindow.ShowInTaskbar = false;
+        try {
+            action(fakeWindow);
+        } finally {
+            fakeWindow.Close();
+            fakeWindow.Dispose();
+        }
+    }
+}
