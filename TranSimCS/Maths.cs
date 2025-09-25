31464d9724a91282586b2d73bea42349e82eaee3
Monniasza <37037712+Monniasza@users.noreply.github.com>
2025-09-25 10:42:10 +0200

    added save dialogs - again

---
+++ TranSimCS/Maths.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranSimCS {
+    public static class Maths {
+        public static readonly float _1_3 = 1 / 3f;
+    }
+}
