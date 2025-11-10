fda298d8f8acc55851a355b1a087732dd6b2e081
Monniasza <37037712+Monniasza@users.noreply.github.com>
2025-11-10 18:33:08 +0100

    readd misignored files

---
+++ TranSimCS/Model/OBJ/ICloneable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TranSimCS.Model.OBJ {
+    public interface ICloneable<T>: ICloneable {
+        public new T Clone();
+
+        object ICloneable.Clone() => Clone();
+    }
+}
