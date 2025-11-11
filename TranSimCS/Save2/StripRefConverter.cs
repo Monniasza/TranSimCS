fa11912067b120e4932ddf6812e0f51c7b15624e
Monniasza <37037712+Monniasza@users.noreply.github.com>
2025-11-11 21:17:53 +0100

    fixed parsing of lane strips

--- TranSimCS/Save2/StripRefConverter.cs
+++ TranSimCS/Save2/StripRefConverter.cs
@@ -12,18 +12,20 @@ using TranSimCS.Worlds;
 namespace TranSimCS.Save2 {
     public class StripRefConverter(TSWorld world) : JsonConverter<LaneStrip> {
         public override LaneStrip? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
-            var readerX = reader;
-            if (readerX.TokenType == JsonTokenType.Null) return null;
-            readerX.Read();
-            if (readerX.TokenType == JsonTokenType.Null) return null;
+            LaneEnd startLaneEnd, endLaneEnd;
 
-            var elements = new List<LaneEnd>(2);
             var laneEndConverter = new LaneEndConverter(world);
-            JsonProcessor.ReadJsonArrayProperties(ref reader, (ref reader0, idx) => {
-                elements.Add(laneEndConverter.Read(ref reader0, typeof(LaneEnd), options));
-            });
-            if (elements.Count < 0) JsonProcessor.Fail(reader, "Fewer than 2 elements for the lane strip");
-            return world.FindLaneStrip(elements[0], elements[1]);
+
+            reader.Read();
+            if (reader.TokenType == JsonTokenType.Null) return null;
+            if (reader.TokenType != JsonTokenType.StartArray) JsonProcessor.FailTokenTypes(ref reader, JsonTokenType.Null, JsonTokenType.StartArray);
+
+            startLaneEnd = laneEndConverter.Read(ref reader, typeof(LaneEnd), options);
+            endLaneEnd = laneEndConverter.Read(ref reader, typeof(LaneEnd), options);
+
+            JsonProcessor.AssertTokenType(ref reader, JsonTokenType.EndArray);
+            
+            return world.GetOrMakeLaneStrip(startLaneEnd, endLaneEnd);
         }
 
         public override void Write(Utf8JsonWriter writer, LaneStrip value, JsonSerializerOptions options) {
