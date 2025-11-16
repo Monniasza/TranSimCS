6c9f041a55c36533c3cf2e8e7d76e69ebe3de2bf
Monniasza <37037712+Monniasza@users.noreply.github.com>
2025-11-16 19:12:50 +0100

    merged ModelOld into Model

---
+++ TranSimCS/Model/OBJ/MeshComplexMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranSimCS.Collections;
+
+namespace TranSimCS.Model.OBJ {
+    public static class MeshComplexMethods {
+        public static void AddAll(this MeshComplex dest, MeshComplex src) => dest.Elements.AddRange(src.Elements);
+    }
+}
