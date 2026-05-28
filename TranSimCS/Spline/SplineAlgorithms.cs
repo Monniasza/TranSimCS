e8e5be40b5728f2cc0e74f335ebacd7f9f45025e
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-05-28 20:31:28 +0200

    implemented anisotropic spline algorithm

---
+++ TranSimCS/Spline/SplineAlgorithms.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using TranSimCS.Geometry;
+
+namespace TranSimCS.Spline {
+    public class SplineAlgorithms {
+        public static Bezier3 AnisotropicSpline(Vector3 start, Vector3 startTangent, Vector3 end, Vector3 endTangent) {
+            var flattenedStart = start.ToX0Z();
+            var flattenedStartTangent = startTangent.ToX0Z();
+            var flattenedEnd = end.ToX0Z();
+            var flattenedEndTangent = endTangent.ToX0Z();
+
+            flattenedStartTangent.Normalize();
+            flattenedEndTangent.Normalize();
+            flattenedStartTangent *= startTangent.Length();
+            flattenedEndTangent *= endTangent.Length();
+
+            var flattenedSpline = GeometryUtils.GenerateJoinSpline(flattenedStart, flattenedEnd, flattenedStartTangent, flattenedEndTangent);
+            var anisotropicStartTangent = flattenedSpline.b - flattenedSpline.a;
+            var anisotropicEndTangent = flattenedSpline.c - flattenedSpline.d;
+
+            //Adjust the anisotropic tangents' Y to make them inline to source tangents
+            // For each spline (anisoTangent, originalTangent): len(anisoTangent.xz) / newY = len(originalTangent.xz) / originalTangent.y
+            // By rearranging, 1/newY = len(originalTangent.xz) / (originalTangent.y * len(anisoTangent.xz))
+            // newY = originalTangent.y * len(anisoTangent.xz) / len(originalTangent.xz)
+
+            anisotropicStartTangent.Y = startTangent.Y * anisotropicStartTangent.ToX0Z().Length() / startTangent.ToX0Z().LengthSquared();
+              anisotropicEndTangent.Y =   endTangent.Y *   anisotropicEndTangent.ToX0Z().Length() /   endTangent.ToX0Z().LengthSquared();
+
+            flattenedSpline.a.Y = start.Y;
+            flattenedSpline.d.Y = end.Y;
+            flattenedSpline.b = flattenedSpline.a + anisotropicStartTangent;
+            flattenedSpline.c = flattenedSpline.d + anisotropicEndTangent;
+
+            return flattenedSpline;
+        }
+    }
+}
