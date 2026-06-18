2dbdc380dfe61c321bac45e677707d7babb45bec
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-06-18 16:45:49 +0200

    Add conditional methods to SegmentHalf

--- TranSimCS/Roads/Strip/SegmentHalf.cs
+++ TranSimCS/Roads/Strip/SegmentHalf.cs
@@ -1,13 +1,21 @@
-﻿namespace TranSimCS.Roads.Strip {
+﻿using System;
+
+namespace TranSimCS.Roads.Strip {
     public enum SegmentHalf {
         Start, // Represents the left half of a road segment
         End // Represents the right half of a road segment
     }
     public static class SegmentHalfMethods {
-        public static int Discriminant(this SegmentHalf half) {
-            if (half == SegmentHalf.Start) return -1;
-            if (half == SegmentHalf.End) return 1;
-            return 0;
-        }
+        public static int Discriminant(this SegmentHalf half) => half.GetConditionalOrInvalid(-1, 1, 0);
+        public static T GetConditional<T>(this SegmentHalf half, T start, T end) => half switch {
+            SegmentHalf.Start => start,
+            SegmentHalf.End => end,
+            _ => throw new ArgumentException("Invalid segment half specified."),
+        };
+        public static T GetConditionalOrInvalid<T>(this SegmentHalf half, T start, T end, T invalid) => half switch {
+            SegmentHalf.Start => start,
+            SegmentHalf.End => end,
+            _ => invalid,
+        };
     }
 }
