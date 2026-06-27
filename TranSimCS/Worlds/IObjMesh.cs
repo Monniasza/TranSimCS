de22e475eba706014dc588b0bc2d0416efe78ee3
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-06-27 21:32:02 +0200

    add selection mesh methods to IObjMesh

--- TranSimCS/Worlds/IObjMesh.cs
+++ TranSimCS/Worlds/IObjMesh.cs
@@ -4,11 +4,19 @@ using TranSimCS.SceneGraph;
 namespace TranSimCS.Worlds {
     public interface IObjMesh {
         public MultiMesh GetMesh();
+        public MultiMesh? GetSelection0();
         public void InvalidateMesh();
     }
     public interface IObjMesh<T>: IObjMesh where T: Obj, IObjMesh<T> {
         public MeshGenerator<T> Mesh { get; }
+        public MeshGenerator<T>? SelectionMesh { get => null; }
         MultiMesh IObjMesh.GetMesh() => Mesh.GetMesh();
         void IObjMesh.InvalidateMesh() => Mesh.Invalidate();
+        MultiMesh? IObjMesh.GetSelection0() => SelectionMesh?.GetMesh();
+    }
+
+    public static class ObjMeshMethods {
+        public static MultiMesh GetSelection(this IObjMesh obj)
+            => obj.GetSelection0() ?? obj.GetMesh();
     }
 }
