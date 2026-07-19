138528e1df4047b52e7319f3c934f707b72cf375
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-07-19 13:20:38 +0200

    extract LaneStripEnd to a new file

--- TranSimCS/Roads/Strip/LaneStrip.cs
+++ TranSimCS/Roads/Strip/LaneStrip.cs
@@ -14,28 +14,6 @@ using TranSimCS.Spline;
 using TranSimCS.Worlds;
 
 namespace TranSimCS.Roads.Strip {
-    public struct LaneStripEnd(LaneStrip strip, SegmentHalf half) : IDraggableObj, IRoadElement {
-        public LaneStrip strip = strip;
-        public SegmentHalf half = half;
-        public LaneEnd laneEnd => strip.GetHalf(half);
-
-        //DRAGGING
-        IPosition[] IDraggableObj.DraggableComponents() => ((IDraggableObj)strip).DraggableComponents();
-
-        //ROAD ELEMENT
-        public Guid Guid => strip.road.Guid;
-        public Lane? GetLane() => strip.GetHalf(half).lane;
-        public LaneStrip? GetLaneStrip() => strip;
-        public RoadNode? GetRoadNode() => strip.GetHalf(half).RoadNodeEnd.Node;
-        public RoadStrip? GetRoadStrip() => strip.road;
-        public int XDiscriminant() => 0;
-        public int ZDiscriminant() => half.Discriminant();
-        public LaneEnd? GetLaneEnd() => laneEnd;
-        public RoadNodeEnd? GetNodeEnd() => strip.GetHalf(half).RoadNodeEnd;
-
-        public IPosition[] DraggableComponents() => ((IDraggableObj)strip).DraggableComponents();
-    }
-
     public class LaneStrip : IEquatable<LaneStrip?>, IDraggableObj, IRoadElement {
         //ROAD ELEMENT
         public Guid Guid => road.Guid;
