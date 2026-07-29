4ffe450ce789bf4762bcd79a2609c7a68ca5bc0e
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-07-29 17:44:56 +0200

    added collection to NodeSpec methods

---
+++ TranSimCS/Roads/Node/NodeSpecMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranSimCS.Roads.Node {
+    public static class NodeSpecMethods {
+        public static NodeSpec ToNodeSpec(this IEnumerable<LaneNode> lanes) => new NodeSpec(lanes);
+        public static NodeSpec ToNodeSpec(this IEnumerable<Lane> lanes) => lanes.Select(x => x.LaneNode).ToNodeSpec();
+        public static NodeSpec ToNodeSpec(this IEnumerable<HalfLane> lanes) => lanes.Select(x => x.LaneNode).ToNodeSpec();
+    }
+}
