91bcac63f25fb95f00a3746f0d1bc740624a034b
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-08-26 08:37:29 +0200

    added `IVertexType` to new `Vertex`

--- TranSimCS/SilkNet/Vertex.cs
+++ TranSimCS/SilkNet/Vertex.cs
@@ -11,12 +11,20 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace TranSimCS.SilkNet {
     [StructLayout(LayoutKind.Sequential)]
-    public struct Vertex : IEquatable<Vertex> {
+    public struct Vertex : IEquatable<Vertex>, IVertexType {
         public Vector3 Position;
         public Color Color;
         public Vector2 TexCoord;
         public ushort Material;
         public ushort Emissive;
+
+        private VertexDeclaration _vertexDeclaration = new(
+            new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+            new VertexElement(12, VertexElementFormat.Color, VertexElementUsage.Color, 0),
+            new VertexElement(16, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
+            new VertexElement(24, VertexElementFormat.Short2, VertexElementUsage.BlendIndices, 0),
+        );
+        public VertexDeclaration VertexDeclaration => throw new NotImplementedException();
 
         public Vertex(Vector3 position, Color color, Vector2 texCoord, ushort material = 0, ushort emissive = 0) {
             Position = position;
