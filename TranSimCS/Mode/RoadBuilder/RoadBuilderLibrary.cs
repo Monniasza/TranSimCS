193647f1a952a33dc4ece6999eaace5cf142af62
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-09-20 18:23:44 +0200

    wrote a library skeleton

---
+++ TranSimCS/Mode/RoadBuilder/RoadBuilderLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranSimCS.Roads;
+using TranSimCS.Roads.Node;
+using TranSimCS.Worlds;
+
+namespace TranSimCS.Mode.RoadBuilder {
+    public class RoadBuilderLibrary {
+        public TSWorld World {get; private set;}
+        public Dictionary<string, LaneSpec> LaneSpecs { get; private set; } = [];
+        public Dictionary<string, RoadFinish> RoadFinishes { get; private set; } = [];
+        public Dictionary<string, NodeSpec> NodeSpecs { get; private set; } = [];
+    }
+}
