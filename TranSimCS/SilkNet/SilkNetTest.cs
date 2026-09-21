0cf6ab08c21b68e04d839caa487d9d79936e7915
Monniasza <37037712+Monniasza@users.noreply.github.com>
2026-09-21 18:29:41 +0200

    remove the confusing and unfinished road builder mode

--- TranSimCS/SilkNet/SilkNetTest.cs
+++ TranSimCS/SilkNet/SilkNetTest.cs
@@ -94,7 +94,7 @@ namespace TranSimCS.SilkNet {
                 pickMode, new ModeDemolish(this), new ModeNode(this), new ModeSegment(this),
                 new ModeSection(this), new ModeConnection(this), new ModeMoveIt(this),
                 new ModeReverse(this), new ModeSplit(this), new ModeSpline(this),
-                new ModeTrafficLights(this), new ModeRoadBuilder(this),
+                new ModeTrafficLights(this), //new ModeRoadBuilder(this),
             ];
             _mode = pickMode;
             snappingGrid = new();
